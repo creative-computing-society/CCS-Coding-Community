b93bc4b8b5106a60411c9502aa2fcffdf0735b2d
Sanya <88281057+sanya-mahajan@users.noreply.github.com>
2023-11-28 11:24:45 +0530

    27/11/23

--- Trees/235_lowest_common_ancestor_of_BST.cpp
+++ Trees/235_lowest_common_ancestor_of_BST.cpp
@@ -1,3 +1,7 @@
+
+//recursive  
+// keep descending in the tree until the node satisfies BST property
+// ie; value of left < node < right
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
@@ -7,5 +11,23 @@ public:
             return lowestCommonAncestor(root->right,p,q);
 
         return root;    
+    }
+};
+
+//iterative 
+// same logic 
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(1){
+            if(root->val > p->val && root->val > q->val)
+                root=root->left;
+
+            else if(root->val < p->val && root->val < q->val)
+                root=root->right;   
+            else 
+                break;     
+        }  
+        return root;
     }
 };
