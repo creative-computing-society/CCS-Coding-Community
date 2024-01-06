0ea87b3b6e75df71b5085032fdf1ed1d50c17bc8
Sanya <88281057+sanya-mahajan@users.noreply.github.com>
2024-01-06 23:24:17 +0530

    Create 48_rotate_image.cpp

---
+++ Arrays/48_rotate_image.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+//stl
+    void rotate(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        //transpose
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        //reversal
+
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
