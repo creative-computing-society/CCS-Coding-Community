0e8b391000cf6a2c897d373aa3a91c7526e7cb32
Sanya <88281057+sanya-mahajan@users.noreply.github.com>
2024-01-06 23:29:27 +0530

    6/1/24

--- Arrays/48_rotate_image.cpp
+++ Arrays/48_rotate_image.cpp
@@ -4,6 +4,7 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n=matrix.size();
         //transpose
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<i;j++){
                 swap(matrix[i][j],matrix[j][i]);
